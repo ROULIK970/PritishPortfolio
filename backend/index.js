a0a2fd1651f06bf01560628fd7366475f8d5ebe4
Pritish <bordoloipritish97@gmail.com>
2025-01-29 23:27:58 +0530

    removing socket.io logic and testing

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,7 @@ dotenv.config({ path: "./env" });
 
 
 
-const httpServer = createServer(app); // HTTP server for Socket.IO
-const io = new Server(httpServer, {
-  cors: {
-    origin: "pritish-portfolio-fz59glooy-roulik970s-projects.vercel.app", // Allow all origins for now
-    methods: ["GET", "POST"],
-  },
-});
+
 
 app.use(cors());
 app.use(express.json());
@@ -28,48 +22,12 @@ app.use("/", ChatRouter);
 app.use('/auth', AuthRouter)
 app.use('/contact', ContactRouter)
 
-// Socket.IO Chat Logic
-io.on("connection", (socket) => {
-  console.log("New client connected:", socket.id);
-
-  // Store the owner ID when the owner connects
-  let ownerSocketId = null;
-
-  socket.on("registerOwner", () => {
-    ownerSocketId = socket.id;
-    console.log("Owner registered with socket ID:", ownerSocketId);
-  });
-
-
-    socket.on("chatMessage", (data) => {
-      const { message, senderId } = data;
-
-      if (socket.id !== ownerSocketId) {
-        // Send the client's message to the owner
-        if (ownerSocketId) {
-          io.to(ownerSocketId).emit("chatMessage", { senderId, message });
-        }
-      } else {
-        // Broadcast the owner's message to a specific client
-        io.to(senderId).emit("chatMessage", { senderId: "owner", message });
-      }
-    });
-
-  // Handle client disconnect
-    socket.on("disconnect", () => {
-      if (socket.id === ownerSocketId) {
-        console.log("Owner disconnected:", socket.id);
-        ownerSocketId = null;
-      } else {
-        console.log("Client disconnected:", socket.id);
-      }
-    });
-});
+// Socket.IO Chat Logic to be added
 
 
 connectDB()
   .then(() => {
-    httpServer.listen(process.env.PORT || 3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`Server is running at PORT: ${process.env.PORT}`);
     });
   })
